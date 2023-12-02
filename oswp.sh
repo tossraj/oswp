1babb83c93648961520300c86e3d696675f75345
tossraj <117505322+tossraj@users.noreply.github.com>
2023-12-02 14:07:19 +0530

    Update oswp.sh
    Signed-off-by: tossraj <117505322+tossraj@users.noreply.github.com>

--- oswp.sh
+++ oswp.sh
@@ -20,16 +20,28 @@ updatecore () {
 
 updateplugin () {
     path=$1 allow=$2 force=$3
+    inactive_themes=$(wp theme list --status=inactive --field=name --path=$path $(rootpermissions $allow))
     if [ ! $force ];
     then
         wp plugin delete wp-file-manager hello akismet better-search-replace classic-editor loginizer really-simple-ssl --path=$path $(rootpermissions $allow);
         wp plugin update --all --path=$path $(rootpermissions $allow);
-        wp theme delete $(wp theme list --status=inactive --field=name --path=$path $(rootpermissions $allow)) --path=$path $(rootpermissions $allow);
+        for theme in $inactive_themes; do
+            # Check if the theme is a child theme
+            if ! wp theme is-child-theme $theme --path=$path $(rootpermissions $allow); then
+                # If it's not a child theme, delete it
+                wp theme delete $theme --path=$path $(rootpermissions $allow)
+            fi
+        done
     else
         wp plugin delete wp-file-manager hello akismet better-search-replace classic-editor loginizer really-simple-ssl --path=$path $(rootpermissions $allow);
-        wp plugin install $(wp plugin list --field=name --path=$path $(rootpermissions $allow)) --force --path=$path $(rootpermissions $allow);
-        wp theme delete twentytwenty twentytwentyone twentytwentythree twentytwentytwo --path=$path $(rootpermissions $allow);
-        wp theme install $(wp theme list --field=name --path=$path $(rootpermissions $allow)) --force --path=$path $(rootpermissions $allow);
+        wp plugin install $(wp plugin list --field=name --path=$path $(rootpermissions $allow)) --force --path=$path $(rootpermissions $allow);        
+        for theme in $inactive_themes; do
+            # Check if the theme is a child theme
+            if ! wp theme is-child-theme $theme --path=$path $(rootpermissions $allow); then
+                # If it's not a child theme, delete it
+                wp theme delete $theme --path=$path $(rootpermissions $allow)
+            fi
+        done
     fi
 }
 
